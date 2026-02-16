f596620efa217c857c9ba3ebcabfec9e9b36715e
Anisur Rahman <anisur@zone7.co>
2026-02-16 09:26:06 +0600

    chore(release): add WordPress.org readme and metadata headers

--- woo-filters.php
+++ woo-filters.php
@@ -6,7 +6,12 @@
  * Author: Anisur Rahman
  * Author URI: https://github.com/anisur2805
  * Requires at least: 6.0
+ * Tested up to: 6.8
  * Requires PHP: 7.4
+ * WC requires at least: 7.0
+ * WC tested up to: 10.0
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
